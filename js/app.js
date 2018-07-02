0f2ddae63311e092dd9e966c18d14eb3be599c98
Lauren Hopkins <laurenhop7@gmail.com>
2018-07-02 12:41:46 -0400

    Renders player character

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
+// Class for all entities in game
+
+class Entity {
+    constructor() {
+        this.sprite = 'images/';
+        this.x = 2;
+        this.y = 5;
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x = 200, this.y = 400);
+    }
+}
+
+// Player
+
+class Player extends Entity {
+    constructor() {
+        super();
+        this.sprite += 'char-princess-girl.png';
+    }
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+class Enemy extends Entity {
+    constructor() {
+        super();
+        this.sprite = 'enemy-bug.png';
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -34,6 +38,9 @@ Enemy.prototype.render = function() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+const player = new Player();
+//const enemy = new Enemy();
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
